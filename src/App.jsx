a66e818cb6dd09b60c2885dd98189b2a7e27c6a4
AnirbanNath-dev <anirbaniitian@gmail.com>
2024-01-04 18:43:35 +0530

    Added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import authService from './appwrite/auth'
+import { login, logout } from "./store/authSlice"
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+  const [loading , setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getAccount()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+
+      }
+    })
+    .finally(()=>{
+      setLoading(false)
+    })
+  },[])
+
+
+  return !loading ? (
     <>
-      
+      hello
     </>
-  )
+  ) : null
 }
 
 export default App
